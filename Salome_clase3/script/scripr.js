acc95433051cb9bfc1bf7a76f77ab8dd0a73d3b0
Salome11S <100799019+Salome11S@users.noreply.github.com>
2022-03-02 15:57:07 -0500

    Add files via upload

---
+++ Salome_clase3/script/scripr.js
@@ -0,0 +1,28 @@
+const matricula = 900000
+let creditos, descuento, TotalM;
+
+function LeerDatos() {
+    creditos = Number(prompt('Ingrese cantidad de creditos'))
+}
+LeerDatos()
+Calcular(creditos)
+
+function Calcular(creditos) {
+    if (creditos <= 6) {
+        descuento = matricula * 0.5;
+    }
+    else if (creditos > 6 && creditos <= 10) {
+        descuento = matricula * 0.25
+    }
+    else if (creditos > 10) {
+        descuento = 0;
+    } else {
+        alert('Opción no valida ')
+    }
+
+    TotalM = matricula - descuento
+    return TotalM
+}
+
+console.log(TotalM)
+alert(TotalM)
